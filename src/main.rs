7df3f5a01384004b51dabef519a58b97e63cfa2c
Bogdan Mihai <bogdanm@fb.com>
2023-04-03 18:08:56 +0200

    low-level echo server, 1kb buffer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
+use std::net::SocketAddr;
+
+use tokio::net::TcpListener;
+use tokio::net::TcpStream;
+use tokio::io::AsyncWriteExt;
+use tokio::io::AsyncReadExt;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    println!("chat-tokio-stream");
+
+    let listener: TcpListener = TcpListener::bind("localhost:8484").await.unwrap();
+    let (mut socket, _addr): (TcpStream, SocketAddr) = listener.accept().await.unwrap();
+
+    loop {
+        let mut buffer: [u8; 1024] = [0u8; 1024];  //1KB
+        let bytes_read: usize = socket.read(&mut buffer).await.unwrap();
+        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+    }
 }
